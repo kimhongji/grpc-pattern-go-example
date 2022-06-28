339c7a94ee4ab3282a4ac2748c37eec8020f1345
hayz-de <hayz.de@kakaoenterprise.com>
2022-06-29 00:38:44 +0900

    bulk commit

--- channeltest/main.go
+++ channeltest/main.go
@@ -1,55 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
-func channelTest1() {
-	ch := make(chan int)
-	go func() {
-		fmt.Println("before ..")
-		var i int
-		for {
-			i++
-			ch <- i
-			time.Sleep(1 * time.Second)
-		}
-	}()
-
-	for {
-		fmt.Println("i'm main...")
-		fmt.Println(<- ch)
-		time.Sleep(5 * time.Second)
-	}
-}
-
-func channelTest2(name string, ch chan int){
-	for {
-		fmt.Println(name, " turn")
-		i := <- ch
-		fmt.Println(name, ": ", i)
-		i++
-		ch <- i
-
-		time.Sleep(1 * time.Second)
-	}
-}
-
 func main() {
-	// case 1
-	//channelTest1()
-
-	// case 2
-	//ch := make(chan int)
-	//go channelTest2("player1", ch)
-	//go channelTest2("player2", ch)
-	//
-	//ch <- 1
-	//
-	//for {
-	//	time.Sleep(1 * time.Second)
-	//}
 
 
 }
